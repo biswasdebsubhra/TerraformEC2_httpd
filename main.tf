b0c532c0c0249b3fbac27af5b0a07c8491e9fc96
Debsubhra Biswas <biswasdebsubhra@gmail.com>
2020-04-27 07:26:47 +0000

    Modified the main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 0.11.0"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "org"
+
+    workspaces {
+      prefix = "Terraform-"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
